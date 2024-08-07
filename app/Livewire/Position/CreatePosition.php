9f36ec6e78425dae44f6fd7f6ac88b238e611fa0
Jack Ellis <jackellis1504@gmail.com>
2024-08-07 21:49:43 +0100

    wip
     M app/Livewire/Position/CreatePosition.php
     M resources/views/livewire/position/create-position.blade.php

--- app/Livewire/Position/CreatePosition.php
+++ app/Livewire/Position/CreatePosition.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Position;
 
 use App\Livewire\Forms\PositionForm;
 use App\Models\Sport;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class CreatePosition extends Component
@@ -11,9 +12,33 @@ class CreatePosition extends Component
     public PositionForm $form;
     public ?Sport $sport;
 
+    public const MAX_PERCENT = 100;
+    public const MAX_PREVIEW_PERCENT = 90;
+    public const MIN_PREVIEW_PERCENT = 10;
+    public const SCALE_FACTOR_X = 10;
+    public const SCALE_FACTOR_Y = 7;
+
     public function mount(?Sport $sport)
     {
         $this->sport = $sport;
+    }
+
+    private function boundValue(int $n): float
+    {
+        // lambda(n, ((90-10)*n / 100) + 10)
+        return self::MIN_PREVIEW_PERCENT + ($n * (self::MAX_PREVIEW_PERCENT - self::MIN_PREVIEW_PERCENT))/self::MAX_PERCENT;
+    }
+
+    #[Computed]
+    public function previewXBounded(): float
+    {
+        return $this->boundValue($this->form->previewX);
+    }
+
+    #[Computed]
+    public function previewYBounded(): float
+    {
+        return $this->boundValue($this->form->previewY);
     }
 
     public function render()
--- resources/views/livewire/position/create-position.blade.php
+++ resources/views/livewire/position/create-position.blade.php
@@ -3,16 +3,17 @@
     <textarea wire:model="form.description"></textarea>
     <input type="number" wire:model="form.defaultNumber" min="0">
     <input type="number" wire:model="form.sortOrder" min="0">
-    <input type="range" wire:model.live="form.previewX" min="0" max="100" />
-    <input type="range" wire:model.live="form.previewY" min="0" max="100" />
+    <input type="range" wire:model.live="form.previewX" min="" max="{{ self::MAX_PERCENT }}" />
+    <input type="range" wire:model.live="form.previewY" min="" max="{{ self::MAX_PERCENT }}" />
 
-    ({{ $form->previewX }}, {{ $form->previewY }})
-    <div class="relative min-h-64 bg-green-500">
+    ({{ $this->previewXBounded }}, {{ $this->previewYBounded }})
+    <div class="relative min-h-64 bg-green-500 aspect-[10/7]">
         <div
             class="absolute w-4 h-4 bg-red-950"
             style="
-                bottom: {{ $form->previewY }}%;
-                left: {{ $form->previewX }}%;
+                bottom: {{ $this->previewYBounded }}%;
+                left: {{ $this->previewXBounded }}%;
+                transform: translate(-50%, 50%);
             "
         ></div>
     </div>
